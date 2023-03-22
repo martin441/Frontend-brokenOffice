bc0da33d8f6d582c5ad7ed1d1f907d51e7566653
Julieta Magnago <julietamagnago@gmail.com>
2023-03-22 17:39:04 +0100

    feat: profileView

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,25 +1,28 @@
-import { AppBar, Toolbar } from '@mui/material'
-import { Container } from '@mui/system'
-import React from 'react'
-import { Logo } from './Logo'
-import AccountMenu from './Menu'
+import { AppBar, Toolbar } from "@mui/material";
+import { Container } from "@mui/system";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Logo } from "./Logo";
+import AccountMenu from "./Menu";
 
 export const Navbar = () => {
   return (
-    <AppBar position="sticky" sx={{ mb: 0 }} color='secondary'>
-    <Container maxWidth="xl">
-      <Toolbar
-        disableGutters
-        sx={{
-          width: "100%",
-          display: "inline-flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <Logo/>
-        <AccountMenu />
-      </Toolbar>
-    </Container>
-  </AppBar>
-  )
-}
+    <AppBar position="sticky" sx={{ mb: 0 }} color="secondary">
+      <Container maxWidth="xl">
+        <Toolbar
+          disableGutters
+          sx={{
+            width: "100%",
+            display: "inline-flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Link to="/">
+            <Logo />
+          </Link>
+          <AccountMenu />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
