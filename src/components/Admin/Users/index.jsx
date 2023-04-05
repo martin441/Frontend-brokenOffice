36c2fb28bfb584b7df491cdcc4c92c09069f5a89
Julian <julianrinaudo18@gmail.com>
2023-04-05 16:46:49 -0300

    add radio groups for the filter datagrid admin and superAdm

--- src/components/Admin/Users/index.jsx
+++ src/components/Admin/Users/index.jsx
@@ -1,14 +1,27 @@
 import { Box } from "@mui/system";
+import { useState } from "react";
 import { AddBtn } from "../../../commons/AddBtn";
-import Table from "./Table/index"
+import Table from "./Table/index";
+import RadioGroup from "../../../commons/RadioGroup"
 
 export const AdminView = () => {
+  const [type, setType] = useState(localStorage.getItem("value") || "all");
   return (
     <div>
-      <Box sx={{ display: "flex", justifyContent: 'space-evenly', padding: 2 }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 2,
+          gap: 2,
+        }}
+      >
         <AddBtn href="/admin/users/register" />
+      <RadioGroup setForType={[type, setType]} />
       </Box>
-      <Table />
+      <Table filterForType={type}/>
     </div>
   );
 };
--- src/components/SuperAdmin/Users/index.jsx
+++ src/components/SuperAdmin/Users/index.jsx
@@ -1,17 +1,30 @@
 import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { AddBtn } from "../../../commons/AddBtn";
-import Table from "../../Admin/Users/Table"
+import RadioGroup from "../../../commons/RadioGroup";
+import Table from "../../Admin/Users/Table";
 
 export const SuperAdminView = () => {
-  const userType = useSelector(state => state.user.type)
-  
+  const userType = useSelector((state) => state.user.type);
+  const [type, setType] = useState(localStorage.getItem("type") || "all");
+
   return (
     <div>
-      <Box sx={{ display: "flex", justifyContent: "space-evenly", padding: 2 }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 2,
+          gap: 2,
+        }}
+      >
         <AddBtn href="/superadmin/users/register" />
+        <RadioGroup setForType={[type, setType]} />
       </Box>
-      <Table type={userType}/>
+      <Table type={userType} filterForType={type} />
     </div>
   );
 };
