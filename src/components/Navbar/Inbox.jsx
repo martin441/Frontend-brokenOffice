775d43a76aa87e639bd991054649a4aa9edf3276
Julieta Magnago <julietamagnago@gmail.com>
2023-04-13 20:05:22 +0200

    fix/notificationsAllChats

--- src/components/Navbar/Inbox.jsx
+++ src/components/Navbar/Inbox.jsx
@@ -2,11 +2,14 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-import { ButtonGroup, Divider, Typography } from "@mui/material";
-import { useSelector } from "react-redux";
+import { Badge, ButtonGroup, Divider, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
 import { Box } from "@mui/system";
 import { Notification } from "./Notification";
 import { useNavigate } from "react-router";
+import EmailIcon from "@mui/icons-material/Email";
+import { axiosGetInboxIssuer, axiosGetInboxSolver } from "../../utils/axios";
+import { notificationsIssuer, notificationsSolver } from "../../state/chat";
 
 export default function Inbox() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -16,27 +19,52 @@ export default function Inbox() {
   const user = useSelector((state) => state.user);
   const serviceNot = useSelector((state) => state.chat.notificationsSolver);
   const standardNot = useSelector((state) => state.chat.notificationsIssuer);
+
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const getTotalNotifications = () => {
+    if (user?.type === process.env.REACT_APP_BETA) {
+      if (serviceNot.total && standardNot.total) {
+        return serviceNot.total + standardNot.total;
+      } else if (!serviceNot.total || !standardNot.total) {
+        if (serviceNot.total) return serviceNot.total;
+        if (standardNot.total) return standardNot.total;
+      } else {
+        return 0;
+      }
+    } else {
+      return standardNot.total;
+    }
+  };
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
+    if (user) {
+      if (user?.type === process.env.REACT_APP_BETA) {
+        axiosGetInboxSolver().then((res) => dispatch(notificationsSolver(res)));
+        axiosGetInboxIssuer().then((res) => dispatch(notificationsIssuer(res)));
+      } else {
+        axiosGetInboxIssuer().then((res) => dispatch(notificationsIssuer(res)));
+      }
+    }
   };
 
   const handleClose = () => {
     setAnchorEl(null);
   };
 
-  const  handleNotificationClick = (id) => {
+  const handleNotificationClick = (id) => {
     if (user?.type === process.env.REACT_APP_BETA) {
-        if (serviceOption === "issued") {
-          navigate(`/user/ticket/${id}`);
-        } else {
-          navigate(`/service/ticket/${id}`);
-        }
-      } else {
+      if (serviceOption === "issued") {
         navigate(`/user/ticket/${id}`);
+      } else {
+        navigate(`/service/ticket/${id}`);
       }
-  }
+    } else {
+      navigate(`/user/ticket/${id}`);
+    }
+  };
 
   return (
     <div>
@@ -47,7 +75,9 @@ export default function Inbox() {
         aria-expanded={open ? "true" : undefined}
         onClick={handleClick}
       >
-        NOTIFICATIONS
+        <Badge badgeContent={getTotalNotifications()} color="primary">
+          <EmailIcon></EmailIcon>
+        </Badge>
       </Button>
       <Menu
         id="basic-menu"
@@ -99,7 +129,9 @@ export default function Inbox() {
                   <>
                     <MenuItem
                       key={index}
-                      onClick={(e) => handleNotificationClick(notification.report)}
+                      onClick={(e) =>
+                        handleNotificationClick(notification.report)
+                      }
                       id={notification?.report}
                     >
                       {notification && (
@@ -119,33 +151,36 @@ export default function Inbox() {
                 <>
                   <MenuItem
                     key={index}
-                    onClick={(e) => handleNotificationClick(notification.report)}
+                    onClick={(e) =>
+                      handleNotificationClick(notification.report)
+                    }
                     id={notification?.report}
                   >
                     {notification && (
-                      <Notification notification={notification}  />
+                      <Notification notification={notification} />
                     )}
                   </MenuItem>
                   <Divider></Divider>
                 </>
               ))
             ) : (
-              <Typography sx={{ p: 1, ml: 1 }}>{serviceNot}..</Typography>
+              <Typography sx={{ p: 1, ml: 1 }}>{serviceNot}...</Typography>
             )}
           </div>
-        ) : (
-          standardNot?.notifications &&
+        ) : standardNot?.notifications ? (
           standardNot.notifications?.map((notification, index) => (
             <>
               <MenuItem
                 key={index}
                 onClick={(e) => handleNotificationClick(notification.report)}
               >
-                {notification && <Notification notification={notification}  />}
+                {notification && <Notification notification={notification} />}
               </MenuItem>
               <Divider></Divider>
             </>
           ))
+        ) : (
+          <Typography sx={{ p: 1, mx: 1 }}>{standardNot}...</Typography>
         )}
       </Menu>
     </div>
