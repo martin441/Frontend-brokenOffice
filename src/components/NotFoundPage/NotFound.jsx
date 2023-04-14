6d616f04d40cf9d6fe2d0fe511793ba27c308d9b
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-04-14 09:14:56 -0300

    fix: update reports

--- src/components/NotFoundPage/NotFound.jsx
+++ src/components/NotFoundPage/NotFound.jsx
@@ -1,25 +1,25 @@
 import { Box, Button, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 
 const NotFoundPage = () => {
   const navigate = useNavigate();
+  const theme = useSelector(state => state.theme.mode)
   return (
-    <div className="header-container-home home-text">
-      <Box sx={{ mt: 4 }}>
-        <Typography variant="h3">Error 404: Page not found</Typography>
-        <Typography variant="h6">
+      <Box className={theme === 'light' ? 'header-container-home home-text' : 'header-container-home-dark home-text'} >
+        <Typography variant="h3" color={"text.primary"}>Error 404: Page not found</Typography>
+        <Typography variant="h6" color={"text.primary"}>
           Sorry, the page you are looking for does not exist.
         </Typography>
         <Button
           variant="contained"
           color="primary"
-          sx={{ borderRadius: 20, mt: 3 }}
+          sx={{ borderRadius: 20, mt: 3, backgroundColor: "white" }}
           onClick={() => navigate("/")}
         >
           HOME PAGE
         </Button>
       </Box>
-    </div>
   );
 };
 
